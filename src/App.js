ed3a61f0f70984bb83c25ebe3de6a9a487d838da
aghamoutsim <aghamoutsim@gmail.com>
2023-06-07 11:23:34 +0500

    react-proops

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
-import Component from './Component';
+import Proops from './Proops';
 
 function App() {
   return (
     <div>
       <h1>This is a Multiplayer Game</h1>
       <h1>My Favorite Game is God of War Ragnarok</h1>
- 
+
+     <Proops name="muhmmad"/>
+
 
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ src/Proops.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Proops(Props)  {
+    return (
+    
+    <h1>Hello, {Props.name}</h1>    
+    );
+  }
+  
+
+
+
+
+  export default Proops;
